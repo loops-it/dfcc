f2e4385924f404e58dcd19dd04a1093b84cccb0c
tj <thinupavani@gmail.com>
2024-06-19 10:52:14 +0530

    update

--- src/public/scripts/botFunctionScript.js
+++ src/public/scripts/botFunctionScript.js
@@ -1087,7 +1087,7 @@ document
                                     </div>`;
                 case "formGroup":
                   const formArray = item.node_data
-                  const allItems = items;
+                  const allItems = data.productOrService;
 
                   // const matchingNodes = allItems.nodes.filter(node => node.node_id === parent_id);
                   console.log("formArray : ", formArray);
