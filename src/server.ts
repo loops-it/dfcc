a1618d2fa67bd3dd244102ea19b3ae0b2a7e77f1
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-06-28 11:53:24 +0530

    leads 2

--- src/server.ts
+++ src/server.ts
@@ -168,6 +168,10 @@ app.get('/agent', (req: Request, res: Response) => {
     const errorMessage = req.flash('error')[0];
     res.render('agent', {successMessage: successMessage,errorMessage: errorMessage});
 });
+app.get('/leads', adminLogged, async (req: Request, res: Response) => {
+  const forms = await prisma.node.findMany({where: { type: "formGroup" }});
+  res.render('leads', {forms: forms});
+});
 app.get('/conversation-history', adminLogged, async (req: Request, res: Response) => {
     const chats = await prisma.botChats.groupBy({
       by: ['message_id']
